1a6a51fd444f9ea96709f6015c676c9d15f084b3
Commander <37365092+Evools@users.noreply.github.com>
2024-01-17 10:49:38 +0600

    create mysqli_fetch_assoc

--- include/category/index.php
+++ include/category/index.php
@@ -1,26 +1,14 @@
 <?php
 
-    require_once "./config/function.php";
+require_once "./config/function.php";
 
-    $arr_category = [
-        "category-html-css" => [
-            "category_name" => "HTML / CSS"
-        ],
-        "category-js" => [
-            "category_name" => "JS"
-        ],
-        "category-php" => [
-            "category_name" => "PHP"
-        ],
-        "category-webmaster" => [
-            "category_name" => "Все для веб мастера"
-        ],
-    ];
+$sql = "SELECT * FROM `category`";
+$result = mysqli_query($connect, $sql);
 
 ?>
 
 <div class="category">
-    <?php foreach ($arr_category as $category_link => $category_name) : ?>
-        <a class="category-link" href="<?= $category_link; ?>"> <?= $category_name["category_name"] ?> </a>
-    <?php endforeach; ?>
+    <?php while ($arr_category = mysqli_fetch_assoc($result)) : ?>
+        <a class="category-link" href="<?= $arr_category['link']; ?>"> <?= $arr_category["title"] ?> </a>
+    <?php endwhile; ?>
 </div>
--- layout/nav.php
+++ layout/nav.php
@@ -4,29 +4,6 @@
             <h2 class="font-[700]">php</h2>
             <span class="blog-span">blog</span>
         </a>
-        <div class="flex items-center gap-4">
-            <button class="bg-[#2D3548] text-white py-3 px-8 rounded-2xl mr-3 transition hover:bg-[#467AE9]">Войти</button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                <path d="M22 22.5L20 20.5M11.5 21.5C12.7476 21.5 13.9829 21.2543 15.1355 20.7769C16.2881 20.2994 17.3354 19.5997 18.2175 18.7175C19.0997 17.8354 19.7994 16.7881 20.2769 15.6355C20.7543 14.4829 21 13.2476 21 12C21 10.7524 20.7543 9.5171 20.2769 8.36451C19.7994 7.21191 19.0997 6.16464 18.2175 5.28249C17.3354 4.40033 16.2881 3.70056 15.1355 3.22314C13.9829 2.74572 12.7476 2.5 11.5 2.5C8.98044 2.5 6.56408 3.50089 4.78249 5.28249C3.00089 7.06408 2 9.48044 2 12C2 14.5196 3.00089 16.9359 4.78249 18.7175C6.56408 20.4991 8.98044 21.5 11.5 21.5V21.5Z" stroke="#2D3548" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            <button class="flex items-center gap-2 py-3 px-8 rounded-2xl bg-white border border-[#F2F4F6]">
-                <span>Рус</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                    <g clip-path="url(#clip0_6_83)">
-                        <path d="M10.4672 0.65625C6.20161 0.65625 2.55942 3.4125 1.21411 7.21875H19.786C18.4079 3.4125 14.7657 0.65625 10.4672 0.65625Z" fill="#F9F9F9"/>
-                        <path d="M10.4672 20.3438C14.7657 20.3438 18.4079 17.5875 19.7532 13.7812H1.21411C2.55942 17.6203 6.20161 20.3438 10.4672 20.3438Z" fill="#ED4C5C"/>
-                        <path d="M1.21406 7.21875C0.853125 8.23594 0.65625 9.35156 0.65625 10.5C0.65625 11.6484 0.853125 12.7641 1.21406 13.7812H19.7859C20.1469 12.7641 20.3438 11.6484 20.3438 10.5C20.3438 9.35156 20.1469 8.23594 19.7859 7.21875H1.21406Z" fill="#428BC1"/>
-                    </g>
-                    <defs>
-                        <clipPath id="clip0_6_83">
-                            <rect width="21" height="21" fill="white"/>
-                        </clipPath>
-                    </defs>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="9" height="9" viewBox="0 0 9 9" fill="none">
-                    <path d="M7.47003 3.3562L5.02503 5.8012C4.73628 6.08995 4.26378 6.08995 3.97503 5.8012L1.53003 3.3562" stroke="#2D3548" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </button>
-        </div>
+        <button class="bg-[#2D3548] text-white py-3 px-8 rounded-2xl mr-3 transition hover:bg-[#467AE9]">Войти</button>
     </div>
 </div>
