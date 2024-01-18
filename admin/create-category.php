fc15b36e579076d2afbfa544498614b3d59f5ff9
Commander <37365092+Evools@users.noreply.github.com>
2024-01-18 14:03:27 +0600

    create delete form

--- admin/create-category.php
+++ admin/create-category.php
@@ -1,5 +1,7 @@
 <?php
 require_once "./config/config.php";
+require_once "./config/function.php";
+
 $title_name = "PHP Blog | Create Category";
 
 $post = $_POST;
@@ -19,11 +21,20 @@ if (isset($post['create-category'])) {
   }
 }
 
-
-require_once "./config/function.php";
-
 $sql = "SELECT * FROM `category`";
 $result = mysqli_query($connect, $sql);
+
+if (isset($_GET['delete'])) {
+  $deleteId = $_GET['delete'];
+
+  $sql_delete = "DELETE FROM `category` WHERE `id` = $deleteId";
+  $query_delete = mysqli_query($connect, $sql_delete);
+
+  if ($query_delete) {
+    header("Location: /create-category");
+    exit();
+  }
+}
 
 ?>
 
@@ -79,7 +90,7 @@ $result = mysqli_query($connect, $sql);
                 </td>
                 <td class="px-6 py-4">
                   <div class="flex gap-4">
-                    <a x-data="{ tooltip: 'Delete' }" href="#">
+                    <a x-data="{ tooltip: 'Delete' }" href="?delete=<?= $table['id']; ?>">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6" x-tooltip="tooltip">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                       </svg>
@@ -93,6 +104,7 @@ $result = mysqli_query($connect, $sql);
                 </td>
               </tr>
             <?php endwhile; ?>
+
 
           </tbody>
 
--- routes.php
+++ routes.php
@@ -6,26 +6,11 @@ get('/', 'pages/index.php');
 get('/contact', 'pages/contact.php');
 
 
-
 get('/admin', 'admin/index.php');
 get('/create-category', 'admin/create-category.php');
+
 post('/create-category', 'admin/create-category.php');
 
-
-//get('/user/$id', 'pages/user');
-//get('/user/$name/$last_name', 'views/full_name.php');
-//get('/product/$type/color/$color', 'product.php');
-//get('/callback', function(){
-//    echo 'Callback executed';
-//});
-//get('/callback/$name', function($name){
-//    echo "Callback executed. The name is $name";
-//});
-//get('/product', '');
-//get('/callback/$name/$last_name', function($name, $last_name){
-//    echo "Callback executed. The full name is $name $last_name";
-//});
-//post('/user', '/api/save_user');
 
 
 any('/404', 'pages/404.php');
